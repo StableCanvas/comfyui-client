11ac49103c2487fdc7126da5fdb6c3efb8f22b03
zhzluke96 <zhzluke96@outlook.com>
2026-02-10 18:18:58 +0800

    fix: add base64 package

--- packages/client/src/utils/arrayBuffer.ts
+++ packages/client/src/utils/arrayBuffer.ts
@@ -1,3 +1,8 @@
+/**
+ * [0,0,0] => "AAAA="
+ * @param arrayBuffer - The ArrayBuffer to convert to base64.
+ * @returns The base64 representation of the ArrayBuffer.
+ */
 export const arrayBufferToBase64 = (arrayBuffer: ArrayBuffer): string => {
   const bytes = new Uint8Array(arrayBuffer);
   let binary = "";
--- packages/client/src/utils/tools.ts
+++ packages/client/src/utils/tools.ts
@@ -1,5 +1,9 @@
 import { WorkflowOutput } from "../workflow/types";
-import { arrayBufferToBase64 } from "./arrayBuffer";
+import { arrayBufferToBase64 as a2b } from "./arrayBuffer";
+
+const b64pkg = (b64: string, type: string) => `data:${type};base64,${b64}`;
+const url2mime = (url: string) =>
+  url.toLowerCase().endsWith(".png") ? "image/png" : "image/jpeg";
 
 export const outToB64Urls = (
   result: WorkflowOutput,
@@ -13,12 +17,12 @@ export const outToB64Urls = (
     result.images.map((x) => {
       switch (x.type) {
         case "buff":
-          return arrayBufferToBase64(x.data);
+          return b64pkg(a2b(x.data), x.mime || "image/png");
         case "url":
           return fetch(x.data)
             .then((res) => res.blob())
             .then(async (blob) =>
-              arrayBufferToBase64(await blob.arrayBuffer()),
+              b64pkg(a2b(await blob.arrayBuffer()), url2mime(x.data)),
             );
         default:
           // @ts-ignore
